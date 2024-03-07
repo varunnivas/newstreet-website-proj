1da4824018297faced3c6a18f957b63e8f38020f
Varun Nivas - NST <varunn@newstreettech.com>
2024-03-07 17:16:59 +0530

    Private route function added

---
+++ src/routes/privateRoute.jsx
@@ -0,0 +1,23 @@
+
+import PropTypes from 'prop-types';
+
+const isAuthenticated = (roleCode, roles) => {
+    return roleCode && roles?.includes(roleCode);
+};
+
+const PrivateRoute = ({ element, roles }) => {
+    if (isAuthenticated('privateRole', roles)) {
+        return element;
+    } else {
+        return (<div>
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+        </div>);
+    }
+};
+
+export default PrivateRoute;
+PrivateRoute.propTypes = {
+    element: PropTypes.element.isRequired,
+    roles: PropTypes.array.isRequired,
+};
--- src/utils/generateRoutes.jsx
+++ src/utils/generateRoutes.jsx
@@ -1,12 +1,33 @@
 import { Route } from 'react-router-dom';
 
+import PrivateRoute from '../routes/privateRoute';
+
 const generateRoutes = (routes) => {
     return routes.map((route, index) => {
         const {
             path,
             component: Component,
+            private: isPrivate,
+            roles,
             exact,
         } = route;
+
+        if (isPrivate) {
+            return (
+                <Route
+                    key={index}
+                    path={path}
+                    exact={exact || ''}
+                    element={
+                        <PrivateRoute
+                            element={<Component />
+                            }
+                            roles={roles}
+                        />
+                    }
+                />
+            );
+        } else {
             return (
                 <>
                     <Route
@@ -17,6 +38,7 @@ const generateRoutes = (routes) => {
                     />
                 </>
             );
+        }
     });
 };
 
