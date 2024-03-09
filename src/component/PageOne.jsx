fdff7490b4477b0d0315fd48c176956db4798636
sushma3456 <sushma.s@newstreettech.com>
2024-03-09 13:03:31 +0530

    theme

---
+++ src/component/PageOne.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+export default function PageOne() {
+  return (
+    <>
+      <Button variant="contained" color="secondary">
+        hgvjhgjy
+      </Button>
+    </>
+  );
+}
