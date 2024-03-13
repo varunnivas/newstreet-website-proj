87813ea0624eae95fd449eeaacc34ef2e3006f5f
WildFire49 <vaishakhsk2052@gmail.com>
2024-03-13 10:05:55 +0530

    Black-Navbar with adjusted animation

--- src/pages/LandingPage/container/Navbar.jsx
+++ src/pages/LandingPage/container/Navbar.jsx
@@ -1,81 +1,78 @@
-import { AppBar, Toolbar, IconButton, Button, useMediaQuery, styled, Grid } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Button, useMediaQuery, styled, Grid, useScrollTrigger } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import theme from '../../../theme';
+import { useState } from 'react';
 
-
-const StyledAppBar = styled(AppBar)(({ theme }) => ({
-  background: `linear-gradient(to right, rgba(244, 67, 54, 0.8), rgba(0, 0, 0, 0.8))`, 
-  opacity: 0.8,
-  }));
-
+const StyledAppBar = styled(AppBar)(
+  ({ theme }) => ({
+    background: `linear-gradient(to right, rgba(244, 67, 54, 0.8), rgba(0, 0, 0, 0.8))`,
+    opacity: 0.87,
+    transition: theme.transitions.create(['transform'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    transform:'translateY(0)',
+  })
+);
 
 const Toolbar1 = styled(Toolbar)(({ theme }) => ({
   display: 'flex',
-  justifyContent: 'space-between', // Distribute content evenly
+  justifyContent: 'space-between',
   alignItems: 'center',
-  backgroundColor: theme.palette.primary.main, // Set background color
+  backgroundColor: theme.palette.primary.main,
   color: theme.palette.getContrastText(theme.palette.primary.main),
-  background: `linear-gradient(to right, ${theme.palette.error.main}, ${theme.palette.common.black})`, 
-  // Set contrasting text color
 }));
 
-const StyledButton = styled(Button)(({ theme }) => ({
-  margin: theme.spacing(2), // Add spacing between buttons
-  padding: theme.spacing(2, 4), // Adjust padding for comfort
-  borderRadius: theme.shape.borderRadius, // Maintain rounded corners
-  fontWeight: 'bold', // Emphasize button text
-  textTransform: 'uppercase', // Convert text to uppercase for a bolder look
+const StyledButton = styled(Button)(({ theme, clicked }) => ({
+  margin: theme.spacing(2),
+  padding: theme.spacing(1.5, 3),
+  borderRadius: theme.shape.borderRadius,
+  fontWeight: 'bold',
+  textTransform: 'uppercase',
   '&:hover': {
-    boxShadow: theme.shadows[1], // Add a subtle hover shadow
+    boxShadow: theme.shadows[1],
   },
 }));
 
-const StyledImageContainer = styled(Grid)(({ theme }) => ({
-  position: 'relative', // Enable positioning for border and shadow
-  overflow: 'hidden', // Prevent content overflowing the box
-  borderRadius: theme.shape.borderRadius, // Match button roundness
-  margin: theme.spacing(0.3), // Add margin for spacing
-  display: 'flex', // Set display to flex for alignment
-  justifyContent: 'center', // Center content horizontally
-  alignItems: 'center', // Center content vertically
-
-  // Border styles
-  border: `1px solid ${theme.palette.secondary.main}`, // Red border
-
-  // Shadow styles
-  boxShadow: theme.shadows[1], // Add a more prominent shadow
-}));
-
 const NavBar = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true);
+  };
+
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const trigger = useScrollTrigger({
+    target: window,
+    threshold: 0,
+    disableHysteresis: true,
+  });
+  const shouldShowAppBar = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
 
   return (
-    <StyledAppBar position="fixed">
+    <StyledAppBar maxWidth={false} position="fixed" style={{ transform: shouldShowAppBar ? 'translateY(0)' : 'translateY(-100%)' }}>
       <Toolbar1>
-        <Grid container alignItems="center"> {/* Grid container for alignment */}
-          <Grid item xs={2}> {/* Logo column */}
-            <StyledImageContainer>
-              <IconButton edge="start" color="inherit" aria-label="menu">
-                {/* Replace the logo image URL */}
-                <img src="https://newstreettech.com/wp-content/uploads/2020/07/New-Street-Tech-footer-logo.png" alt="logo" style={{ width: '190px' }} />
-              </IconButton>
-            </StyledImageContainer>
-          </Grid>
-          <Grid item xs={10}> {/* Buttons column */}
-            {isMobile ? (
-              <IconButton color="inherit">
-                <MenuIcon />
-              </IconButton>
-            ) : (
-              <>
-                <StyledButton color="inherit" >Home</StyledButton>
-                <StyledButton color="inherit">Products</StyledButton>
-                <StyledButton color="inherit">News</StyledButton>
-                <StyledButton color="inherit">Careers</StyledButton>
-                <StyledButton color="inherit">Contact Us</StyledButton>
-              </>
-            )}
-          </Grid>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <img src="https://newstreettech.com/wp-content/uploads/2020/07/New-Street-Tech-footer-logo.png" alt="logo" style={{ width: isMobile ? '190px' : '150px', maxHeight: isMobile ? '150px' : 'auto' }} />
+        </IconButton>
+
+        <Grid item xs={isMobile ? 12 : 10}>
+          {isMobile ? (
+            <IconButton color="inherit" aria-label="open menu">
+              <MenuIcon />
+            </IconButton>
+          ) : (
+            <>
+              <StyledButton color="inherit" onClick={handleClick}>Home</StyledButton>
+              <StyledButton color="inherit" onClick={handleClick}>Products</StyledButton>
+              <StyledButton color="inherit"onClick={handleClick}>News</StyledButton>
+              <StyledButton color="inherit"onClick={handleClick} >Careers</StyledButton>
+              <StyledButton color="inherit" onClick={handleClick}>Contact Us</StyledButton>
+            </>
+          )}
         </Grid>
       </Toolbar1>
     </StyledAppBar>
